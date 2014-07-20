f237d14af3a766fa6cb9d015edcbf93c1036f8fe
Devon Govett <devongovett@gmail.com>
2014-07-20 15:11:41 -0700

    Make sure to export emscripten module

--- compileOgg.sh
+++ compileOgg.sh
@@ -23,3 +23,4 @@ emmake make install
 cd ..
 mkdir -p build
 emcc -O3 -s RESERVED_FUNCTION_POINTERS=50 -s EXPORTED_FUNCTIONS="['_AVOggInit', '_AVOggRead', '_AVOggDestroy']" -I libogg/include -Llibogg/lib -logg src/ogg.c -o build/libogg.js
+echo "module.exports = Module" >> build/libogg.js
