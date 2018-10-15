f0c680999e710e069952ede552e12d241d89cb04
Sergey Kukunin <sergey.kukunin@gmail.com>
2018-10-15 16:05:15 +0300

    Use and compile latest libogg

--- compileOgg.sh
+++ compileOgg.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+set -e
 
 # configure libogg
-cd libogg
+cd ogg
 if [ ! -f configure ]; then
   # generate configuration script
   ./autogen.sh
-  
+
   # -O20 and -04 cause problems
   # see https://github.com/kripken/emscripten/issues/264
-  sed -i '' 's/-O20/-O2/g' configure
-  sed -i '' 's/-O4/-O2/g' configure
-  
-  # finally, run configuration script
-  emconfigure ./configure --prefix="`pwd`" --disable-static
+  sed -i 's/-O20/-O2/g' configure
+  sed -i 's/-O4/-O2/g' configure
 fi
+
+# finally, run configuration script
+emconfigure ./configure --prefix="`pwd`" --disable-shared --enable-static
 
 # compile libogg
 emmake make
-emmake make install
+emmake make -i install
 
 # compile wrapper
 cd ..
 mkdir -p build
-emcc -O3 -s RESERVED_FUNCTION_POINTERS=50 -s EXPORTED_FUNCTIONS="['_AVOggInit', '_AVOggRead', '_AVOggDestroy']" -I libogg/include -Llibogg/lib -logg src/ogg.c -o build/libogg.js
+emcc -O3 -I ogg/include -s WASM=0 -s RESERVED_FUNCTION_POINTERS=50 -s EXPORTED_FUNCTIONS="['_AVOggInit', '_AVOggRead', '_AVOggDestroy']" src/ogg.c ogg/lib/libogg.a -o build/libogg.js
 echo "module.exports = Module" >> build/libogg.js
